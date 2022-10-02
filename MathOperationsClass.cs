7d19b5a397534471b51e372f290ab8c699bb71e5
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2022-10-02 20:13:22 +0300

    New class MathOperationsClass

---
+++ MathOperationsClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator
+{
+    public class MathOperationsClass
+    {
+
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Log(double a)
+        {
+            return Math.Log(a);
+        }
+
+        public int Fac(int a)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                a *= i;
+            }
+            return a;
+        }
+
+        public int Degree(int a, int b)
+        {
+            return (int)Math.Pow(a, b);
+        }
+
+        public int Sqrt(int a)
+        {
+            return (int)Math.Sqrt(a);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.Write("a=");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
         }
     }
 }
